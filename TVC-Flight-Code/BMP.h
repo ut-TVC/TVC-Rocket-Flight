e0e223ba799e1b898d9b35f076d312c090867473
지민 <wlals425315@gmail.com>
2024-09-15 23:29:26 +0900

    docs: BMP 파일에 대한 한글 주석 추가

--- TVC-Flight-Code/BMP.h
+++ TVC-Flight-Code/BMP.h
@@ -3,6 +3,7 @@
  * https://www.adafruit.com/product/2651
  * 
  * Outputs the following data: temperature, athmospheric pressure and altitude (which is calculated with the pressure at sea level)
+ * 다음 데이터를 출력합니다 : 온도, 대기압 , 고도 (해수면을 기준으로 계산합니다. )
  */
 
 #include <Adafruit_BMP280.h>
