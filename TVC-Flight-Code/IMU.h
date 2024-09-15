efe085984af47f8ed2b1229a3d9a3801b25398ff
Maroni03 <jjm0235@naver.com>
2024-09-16 00:05:10 +0900

    PID.h에 대한 한글 주석 추가

--- TVC-Flight-Code/IMU.h
+++ TVC-Flight-Code/IMU.h
@@ -7,11 +7,11 @@
  * Outputs the following data: angular velocity, acceleration and absolute orientation      다음 데이터를 출력: 각속도, 가속도, 절대 방향
  */
 
-#include <Adafruit_BNO055.h>      //include는 무엇을 뜻하는가?
-#include <Adafruit_Sensor.h>
-#include <Wire.h>
-#include <Arduino.h>
-#include "VectorDefinition.h"
+#include <Adafruit_BNO055.h>      //include는 작성된 파일의 내용을 가져와 적용 시키는 역할
+#include <Adafruit_Sensor.h>      //Sensor.h는 Adafruit사의 센서 라이브러리를 사용하여 다른 센서로 교체 시 해당 센서의 라이브러리로의 교환이 필요함
+#include <Wire.h>                 //Wire.h는 아두이노에서 I2C 통신을 쉽게 할 수 있도록 Wire이라는 객체를 제공함.
+#include <Arduino.h>              //Arduino.h는 많은 양의 정의와 표준 항목이 있어 다른 헤더 파일들을 포함하는 포괄적인 헤더 파일임.
+#include "VectorDefinition.h"     //VectorDefinition.h는 해당 TVC-Rocket-Flight에 존재하므로 해당 헤더파일에서 설명
 
 class IMU {
   public:
--- TVC-Flight-Code/PID.h
+++ TVC-Flight-Code/PID.h
@@ -3,11 +3,11 @@
  * PID-제어기
  *
  * The orientation of the motor mount is calculated with two separate PID-Controllers
- * 모터 마운트의 방향은 두 개의 개별 PID 컨트롤러로 계산 됩니다.
+ * 모터 마운트의 방향은 두 개의 개별 PID 컨트롤러로 계산됩니다    
  */
 
-#include <Wire.h>
-#include <Arduino.h>
+#include <Wire.h>         //Wire.h는 아두이노에서 I2C 통신을 쉽게 할 수 있도록 Wire이라는 객체를 제공함.
+#include <Arduino.h>      //Arduino.h는 많은 양의 정의와 표준 항목이 있어 다른 헤더 파일들을 포함하는 포괄적인 헤더 파일임.
 
 class PID {
   public:
@@ -15,14 +15,14 @@ class PID {
     float update(float input);
 
   private:
-    // Proportional gain
+    // Proportional gain      비례 게인 값
     float Kp = 0.0f;
-    // Integral gain
+    // Integral gain      적분 게인 값
     float Ki = 0.0f;
-    // Derivative gain
+    // Derivative gain    미분 게인 값
     float Kd = 0.0f;
 
-    // Loop interval time
+    // Loop interval time     루프 간격 시간
     float deltaTime = 0.0f;
     float error = 0.0f;
     float setpoint = 0.0f;
@@ -30,7 +30,7 @@ class PID {
     float errorDerivative = 0.0f;
     float errorLastCycle = 0.0f;
 
-    // Maximum value of manipulated variable (absolute value)
+    // Maximum value of manipulated variable (absolute value)     조작된 변수의 최대값 (절대값)
     float saturation = 0.0f;
     float output = 0.0f;
 
